e52864f6ec4c4d3e07f4e20a9b88b2e82d7d7abc
alba <alba.fuentes@m47labs.com>
2025-04-09 17:32:58 +0200

    resolve test service

--- moving-coffea/src/types/account/service.type.d.ts
+++ moving-coffea/src/types/account/service.type.d.ts
@@ -2,6 +2,6 @@ import type { AccountModel } from "./model.type";
 
 export interface AccountApiService {
 	getUser(userId: string): Promise<AccountModel.User>;
-	putUser(userId: string): Promise<AccountModel.User>;
+	putUser(user: AccountModel.User): Promise<AccountModel.User>;
 	deleteUser(userId: string): Promise<Response>;
 }
