060d1791a84aa748f4666cc3e938503ef24d6a76
alba <alba.fuentes@m47labs.com>
2025-07-02 14:37:27 +0200

    resolve endpoint to build

--- src/app/api/qr-subscribe/route.ts
+++ src/app/api/qr-subscribe/route.ts
@@ -1,32 +1,38 @@
 import { errors } from "@/helpers/api/errors.helper";
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { StatusError } from "@/types/api.types";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    const { code, token } = req.query;
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url)
+    const token = searchParams.get('token')
+    const code = searchParams.get('code')
 
     if (!token) {
         const error = new Error(errors[500].message);
         error.cause = errors[500].cause;
-        return res.status(500).json({ message: error });
+        const errorStatus: StatusError = { ...error, statusCode: 500 };
+        throw errorStatus;
     }
+    const stream = new ReadableStream({
+        start(controller) {
+            const encoder = new TextEncoder()
 
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        Connection: 'keep-alive',
+            setTimeout(() => {
+                const event = {
+                    code: code,
+                    redeemed: true,
+                }
+                const payload = `data: ${JSON.stringify(event)}\n\n`
+                controller.enqueue(encoder.encode(payload))
+                controller.close()
+            }, 5000)
+        },
     })
 
-    const timeout = setTimeout(() => {
-        const event = {
-            code: code,
-            redeemed: true,
-        }
-
-        res.write(`data: ${JSON.stringify(event)}\n\n`)
-    }, 5000)
-
-    req.on('close', () => {
-        clearTimeout(timeout)
-        res.end()
+    return new Response(stream, {
+        headers: {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            Connection: 'keep-alive',
+        },
     })
 }
